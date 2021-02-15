1e4a24c6ad6d044f5c72b839fdf80fc4f277135d
Ryan Filler <ryanfiller89@gmail.com>
2021-02-15 13:40:32 -0600

    initial commit, follow this walkthrough - https://css-tricks.com/eleventy-starter-with-tailwind-css-alpine-js/

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setUseGitIgnore(false)
+
+  eleventyConfig.addWatchTarget("./_tmp/style.css")
+
+  eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" })
+
+  eleventyConfig.addShortcode("version", function () {
+    return String(Date.now())
+  })
+}
